982b3ebc4d2f07e07ef7fcec95a002ba3df59668
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2025-02-19 17:17:17 +0100

    Initial commit

---
+++ print.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+	<p>Hello, I am <?php print("Jacoby");?></p>
+	<p>Here is a link:
+		<?php
+			// This is a comment
+			print("<a href=\"https://www.jacobreinikainen.com\">jacobreinikainen.com</a>")
+		?>
+	</p>
+	<p>
+		Here is a link to another php-file:
+		<?php
+			// This is a multiline comment
+			print("<a href=\"phpinfo.php\">php info</a>")
+		?>
+	</p>
+</body>
