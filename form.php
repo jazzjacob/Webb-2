c14f143df1bdd0ce1a8793e81e7e4710a562f03e
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2025-02-22 11:34:06 +0100

    simple form

---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<p>Hello this is the form page</p>
+		<form action="handle_form.php" method="post">
+		  <label for="name">Enter your name: </label>
+			<input type="text" name="name" />
+			<input type="submit" value="Send my feedback!" />
+		</form>
+	</body>
+</html>
---
+++ handle_form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+		
+			$_name = $_POST['name'];
+		
+			print("<p>Hello there, $_name!</p>");
+		?>
+	</body>
+</html>
